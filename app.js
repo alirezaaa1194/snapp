e88f26b413523821ea6e187f35601a8b8990b506
Alireza <alirezaaa1194@gmail.com>
2023-01-12 15:22:19 +0330

    add footer

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let xcircle = document.getElementById("xcircle");
 let seemorebtn = document.getElementById("seemorebtn");
 let section4 = document.getElementById("section4");
 let section7=document.querySelector('.section7_con');
+let allbtn=document.querySelector('.all-btn');
 function checkinputvalue1() {
   if (phone.value.length > 0) {
     fillcircle.style.display = "none";
@@ -64,9 +65,28 @@ if(screen.availWidth>750 && screen.availWidth<1100){
 if(screen.availWidth>1100){
   container4.classList.remove('position-absolute')
 }
-if (window.matchMedia("(orientation:landscape)").matches) {
+if (screen.availWidth<800 && window.matchMedia("(orientation:landscape)").matches) {
+  container4.classList.add('position-absolute')
   section5.classList.remove('container')
   section5.classList.add('container-fluid')
   section7.classList.remove('container')
   section7.classList.add('container-fluid')
+}
+let offcanvas_counter=false;
+allbtn.addEventListener('click', function(){
+  if(offcanvas_counter==false){
+    allbtn.style.paddingBottom="30px"
+    allbtn.style.backgroundColor="#fff"
+    allbtn.style.transition='2s'
+    offcanvas_counter=true;
+  }
+  else{
+    allbtn.style.paddingBottom="0px"
+    allbtn.style.background="none"
+    allbtn.style.transition='.1s'
+    offcanvas_counter=false
+  }
+})
+function menuBtnFunction(menuBtn) {
+  menuBtn.classList.toggle("active");
 }
